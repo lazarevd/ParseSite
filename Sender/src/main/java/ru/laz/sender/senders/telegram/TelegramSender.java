681d5ccf0df18063aae1b1861989d79e9213cd6c
lazarevd <lazarevd@gmail.com>
2019-12-20 14:57:21 +0300

    prop place

--- Sender/src/main/java/ru/laz/sender/senders/telegram/TelegramSender.java
+++ Sender/src/main/java/ru/laz/sender/senders/telegram/TelegramSender.java
@@ -19,7 +19,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@PropertySource("classpath:telegram.properties")
+@PropertySource("classpath:config/telegram.properties")
 public class TelegramSender {
 
     private Set<Integer> process = new HashSet<>();
--- build.cmd
+++ build.cmd
@@ -2,7 +2,9 @@ cd /root/java/Build/ParseSite/
 git fetch
 git pull
 mvn clean package
-cp -r Parser/target/{config/,Parser-0.1.0.jar} /root/java/Parser/
-cp -r Sender/target/{config/,Sender-0.1.0.jar} /root/java/Sender/
+cp -r Parser/target/{Parser-0.1.0.jar} /root/java/Parser/
+cp -r Sender/target/{Sender-0.1.0.jar} /root/java/Sender/
 cd /root/java/Parser/
 java -jar Parser-0.1.0.jar
+cd /root/java/Sender/
+java -jar Sender-0.1.0.jar
