21155881541efa3ea6a354f0806addf807512072
lazarevd <lazarevd@gmail.com>
2019-12-20 17:07:26 +0300

    parse_mode

--- Sender/src/main/java/ru/laz/sender/senders/telegram/TelegramDTO.java
+++ Sender/src/main/java/ru/laz/sender/senders/telegram/TelegramDTO.java
@@ -3,6 +3,7 @@ package ru.laz.sender.senders.telegram;
 public class TelegramDTO {
     int chat_id;
     String text;
+    String parse_mode = "HTML";
 
     public TelegramDTO(int chat_id, String text) {
         this.chat_id = chat_id;
@@ -25,5 +26,13 @@ public class TelegramDTO {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public void setParse_mode(String parse_mode) {
+        this.parse_mode = parse_mode;
+    }
+
+    public String getParse_mode() {
+        return parse_mode;
     }
 }
