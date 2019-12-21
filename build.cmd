6852183e436a37dff0427e6af7a1b41a1d041683
lazarevd <lazarevd@gmail.com>
2019-12-21 22:51:49 +0300

    конфиг указывается в свойствах запуска jar

--- build.cmd
+++ build.cmd
@@ -5,4 +5,4 @@ mvn clean package
 cp -f /root/java/Build/ParseSite/Parser/target/Parser-0.1.0.jar /root/java/Parser/Parser-0.1.0.jar
 cp -f /root/java/Build/ParseSite/Sender/target/Sender-0.1.0.jar /root/java/Sender/Sender-0.1.0.jar
 /usr/bin/java -jar /root/java/Sender/Sender-0.1.0.jar --spring.config.location=/root/java/Sender/config/telegram.properties,/root/java/Sender/config/application.properties
-/usr/bin/java -jar /root/java/Parser/Parser-0.1.0.jar --spring.config.location=/root/java/Sender/config/application.properties
+/usr/bin/java -jar /root/java/Parser/Parser-0.1.0.jar --spring.config.location=/root/java/Parser/config/application.properties
