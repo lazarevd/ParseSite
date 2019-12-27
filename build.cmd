de9281601fd38b2616f317b3586a9ac68743ce73
lazarevd <lazarevd@gmail.com>
2019-12-27 17:05:25 +0300

    cmd

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@ cd /root/java/Build/ParseSite/
 git fetch
 git pull
 mvn clean package
-cp -f /root/java/Build/ParseSite/ParserMosfarr/target/Parser-0.1.0.jar /root/java/Parser/Parser-0.1.0.jar
+cp -f /root/java/Build/ParseSite/ParserMosfarr/target/ParserMosfarr.jar /root/java/ParserMosfarr/ParserMosfarr.jar
 cp -f /root/java/Build/ParseSite/Sender/target/Sender-0.1.0.jar /root/java/Sender/Sender-0.1.0.jar
 /usr/bin/java -jar /root/java/Sender/Sender-0.1.0.jar --spring.config.location=/root/java/Sender/config/telegram.properties,/root/java/Sender/config/application.properties
-/usr/bin/java -jar /root/java/Parser/Parser-0.1.0.jar --spring.config.location=/root/java/Parser/config/application.properties
+/usr/bin/java -jar /root/java/ParserMosfarr/ParserMosfarr.jar --spring.config.location=/root/java/ParserMosfarr/config/application.properties
